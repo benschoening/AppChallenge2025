e697b6ff91726a5cf5b670d02ede5365f48e5db9
bjs14 <benschoening@icloud.com>
2025-03-10 17:43:34 -0700

    childcare location extraction and merge of non-spatial data

---
+++ backend/filter_childcare_locations.py
@@ -0,0 +1,7 @@
+childcare_df = pd.read_csv("childcare_locations.csv")
+
+#Filtering Vancouver Child Care Locations (2,000 + -> approx. 500)
+dataFrame = childcare_df[childcare_df['CITY'].str.contains('Vancouver')]
+
+#Checking
+print(childcare_df.head())
